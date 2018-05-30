e97b9d7b968cc424ff5ed106593205d260703636
임동완 <limson_95@naver.com>
2018-05-30 19:41:23 +0900

    closes #5

---
+++ functions/index.js
@@ -0,0 +1,18 @@
+const functions = require('firebase-functions');
+const express = require('express');
+
+const app = express();
+
+app.get('/timestamp', (request,response) => {
+    response.send(`${Date.now()}`);
+})
+
+app.get('/timestamp-cached', (request,response) => {
+    response.set('Cached-Control','public,max-age=300,s-max-age=600')
+    response.send(`${Date.now()}`);
+})
+
+ // Create and Deploy Your First Cloud Functions
+ // https://firebase.google.com/docs/functions/write-firebase-functions
+
+ exports.app = functions.https.onRequest(app);
